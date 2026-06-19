a573a6ab3cd50875a8e5aef774987a364191ee71
shawn-lindberg <shawn.lindberg@gmail.com>
2026-06-18 21:32:34 -0500

    Report loading. (#5)
    * Integrated report loading support from sandman_web. This provides more robust support for loading report files.

--- src/sandman_core/reports.py
+++ src/sandman_core/reports.py
@@ -4,7 +4,6 @@ Reports are automatically generated based on activity.
 """
 
 import collections
-import dataclasses
 import json
 import logging
 import pathlib
@@ -17,17 +16,266 @@ from . import time_util
 _logger = logging.getLogger("sandman.report")
 
 
-type _ReportEventInfo = typing.Mapping[
+type ReportEventInfo = typing.Mapping[
     str, typing.Mapping[str, int | str] | int | str
 ]
 
 
-@dataclasses.dataclass
-class _ReportEvent:
+class ReportEvent:
     """An event for a report file."""
 
-    when: whenever.ZonedDateTime
-    info: _ReportEventInfo
+    def __init__(self) -> None:
+        """Initialize the report event."""
+        self.__when: whenever.ZonedDateTime | None = None
+        self.__info: ReportEventInfo = {}
+
+    @property
+    def when(self) -> whenever.ZonedDateTime | None:
+        """Get the when."""
+        return self.__when
+
+    @when.setter
+    def when(self, when: whenever.ZonedDateTime) -> None:
+        """Set the when."""
+        if isinstance(when, whenever.ZonedDateTime) == False:
+            raise TypeError("When must be a zoned date/time.")
+
+        self.__when = when
+
+    @property
+    def info(self) -> ReportEventInfo:
+        """Get the info."""
+        return self.__info
+
+    @info.setter
+    def info(self, info: ReportEventInfo) -> None:
+        """Set the info."""
+        if isinstance(info, dict) == False:
+            raise TypeError("Info must be a ReportEventInfo.")
+
+        # This will be more robust if the info is a concrete type.
+        if info == {}:
+            raise ValueError("Info must not be an empty dictionary.")
+
+        self.__info = info
+
+    def is_valid(self) -> bool:
+        """Check whether this is a valid report event."""
+        if self.__when is None:
+            return False
+
+        # This will be a lot more robust if the info becomes a concrete class.
+        if self.__info == {}:
+            return False
+
+        return True
+
+    def __eq__(self, other: object) -> bool:
+        """Check whether this event and another have equal values."""
+        if not isinstance(other, ReportEvent):
+            return NotImplemented
+
+        return (self.__when == other.__when) and (self.__info == other.__info)
+
+    @classmethod
+    def parse_from_string(
+        cls, event_string: str, filename: str
+    ) -> typing.Self:
+        """Parse the event from a (JSON) string."""
+        event = cls()
+
+        try:
+            event_json = json.loads(event_string)
+
+        except json.JSONDecodeError:
+            _logger.warning(
+                "JSON error decoding event for report file '%s'.",
+                filename,
+            )
+            return event
+
+        try:
+            event.when = whenever.ZonedDateTime.parse_iso(event_json["when"])
+
+        except KeyError:
+            _logger.warning(
+                "Missing 'when' key in event in report file '%s'.",
+                filename,
+            )
+            return event
+
+        except (TypeError, ValueError):
+            _logger.warning(
+                "Invalid when '%s' in event in report file '%s'.",
+                str(event_json["when"]),
+                filename,
+            )
+            return event
+
+        try:
+            info = event_json["info"]
+
+        except KeyError:
+            _logger.warning(
+                "Missing 'info' key in event in report file '%s'.",
+                filename,
+            )
+
+        else:
+            if isinstance(info, dict) == True:
+                try:
+                    event.info = dict(info)
+
+                except ValueError:
+                    _logger.warning(
+                        "Invalid info '%s' in event in report file '%s'.",
+                        str(info),
+                        filename,
+                    )
+
+            else:
+                _logger.warning(
+                    "Invalid info '%s' in event in report file '%s'.",
+                    str(info),
+                    filename,
+                )
+
+        return event
+
+
+class Report:
+    """All of the information from a report file."""
+
+    def __init__(self) -> None:
+        """Initialize the report."""
+        self.__version = -1
+        self.__start: whenever.ZonedDateTime | None = None
+        self.__events: list[ReportEvent] = []
+
+    @property
+    def version(self) -> int:
+        """Get the version."""
+        return self.__version
+
+    @version.setter
+    def version(self, version: int) -> None:
+        """Set the version."""
+        if isinstance(version, int) == False:
+            raise TypeError("Version must be an integer.")
+
+        if version < 0:
+            raise ValueError("Cannot set a negative version.")
+
+        self.__version = version
+
+    @property
+    def start(self) -> whenever.ZonedDateTime | None:
+        """Get the start."""
+        return self.__start
+
+    @start.setter
+    def start(self, start: whenever.ZonedDateTime) -> None:
+        """Set the start."""
+        if isinstance(start, whenever.ZonedDateTime) == False:
+            raise TypeError("Start must be a zoned date/time.")
+
+        self.__start = start
+
+    @property
+    def events(self) -> list[ReportEvent]:
+        """Get the events."""
+        return self.__events
+
+    def append_event(self, event: ReportEvent) -> None:
+        """Add an event to the end."""
+        self.__events.append(event)
+
+    def is_valid(self) -> bool:
+        """Check whether this is a valid report."""
+        if self.__version < 0:
+            return False
+
+        if self.__start is None:
+            return False
+
+        return True
+
+    @classmethod
+    def parse_from_file(cls, filename: str) -> typing.Self:
+        """Parse a report from a file."""
+        report = cls()
+
+        try:
+            with open(filename) as file:
+                report_lines = file.readlines()
+
+        except FileNotFoundError as error:
+            _logger.error("Could not find report file '%s'.", filename)
+            raise error
+
+        num_lines = len(report_lines)
+
+        if num_lines == 0:
+            _logger.warning("Report file '%s' is empty.", filename)
+            return report
+
+        # The first line is expected to be the header.
+        try:
+            header_json = json.loads(report_lines[0])
+
+        except json.JSONDecodeError:
+            _logger.error(
+                "JSON error decoding header for report file '%s'.",
+                filename,
+            )
+            return report
+
+        try:
+            report.version = header_json["version"]
+
+        except KeyError:
+            _logger.error("Missing version in report file '%s'.", filename)
+            return report
+
+        except (TypeError, ValueError):
+            _logger.error(
+                "Invalid version '%s' in report file '%s'.",
+                str(header_json["version"]),
+                filename,
+            )
+            return report
+
+        # Don't support loading reports older than version 4.
+        if report.version < 4:
+            return report
+
+        try:
+            report.start = whenever.ZonedDateTime.parse_iso(
+                header_json["start"]
+            )
+
+        except KeyError:
+            _logger.error("Missing start in report file '%s'.", filename)
+            return report
+
+        except (TypeError, ValueError):
+            _logger.error(
+                "Invalid start '%s' in report file '%s'.",
+                str(header_json["start"]),
+                filename,
+            )
+            return report
+
+        # Load the events.
+        for line_index in range(1, num_lines):
+            event = ReportEvent.parse_from_string(
+                report_lines[line_index], filename
+            )
+
+            if event.is_valid() == True:
+                report.append_event(event)
+
+        return report
 
 
 class ReportManager:
@@ -43,7 +291,7 @@ class ReportManager:
         self.__reports_dir = base_dir + "reports/"
         # Eventually this should be configurable.
         self.__report_start_hour = 17
-        self.__pending_events = collections.deque[_ReportEvent]()
+        self.__pending_events = collections.deque[ReportEvent]()
 
     def process(self) -> None:
         """Process reports."""
@@ -138,7 +386,7 @@ class ReportManager:
 
         _logger.info("Created report file '%s'.", str(report_file_name))
 
-    def __add_event(self, info: _ReportEventInfo) -> None:
+    def __add_event(self, info: ReportEventInfo) -> None:
         """Add an event with the given info at the current time."""
         try:
             curr_time = self.__time_source.get_current_time()
@@ -147,10 +395,17 @@ class ReportManager:
             _logger.warning("Cannot add events without a valid time.")
             return
 
-        event = _ReportEvent(curr_time, info)
+        event = ReportEvent()
+        event.when = curr_time
+        event.info = info
+
+        if event.is_valid() == False:
+            _logger.warning("Try to add an invalid event %s.", str(event))
+            return
+
         self.__pending_events.append(event)
 
-    def __pop_event(self) -> _ReportEvent | None:
+    def __pop_event(self) -> ReportEvent | None:
         """Pop an event from the queue if there is one.
 
         Returns the event or None if the queue is empty.
@@ -163,8 +418,11 @@ class ReportManager:
 
         return event
 
-    def __write_event(self, event: _ReportEvent) -> None:
+    def __write_event(self, event: ReportEvent) -> None:
         """Write the event to the appropriate file."""
+        if event.when is None:
+            return
+
         self.__maybe_create_report_file(event.when)
 
         report_name = self.__get_report_name_from_time(event.when)
